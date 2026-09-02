e676847400743e3aa3cf3f733e853a1a187bc5c5
Roosevelt <rooseveltmkr@gmail.com>
2026-09-02 12:19:23 +0100

    Add 'Géopolitique' niche

--- src/api/routes/admin.py
+++ src/api/routes/admin.py
@@ -529,7 +529,7 @@ KNOWN_NICHES = [
     "Philosophie", "Philosophie Stoïcienne", "Philosophie de Machiavel", "Philosophie de Napoleon Hill",
     "Prière", "Bouddhisme", "Islam", "Méditation", "Motivation", "Développement Personnel",
     "Psychologie de Carl Jung", "Astuces Maison", "Business", "Cuisine", "Finance", "Faits Divers",
-    "Football", "Histoire Africaine", "Histoire Européenne", "Santé & Bien-être", "Science", "Astronomie & Espace",
+    "Football", "Géopolitique", "Histoire Africaine", "Histoire Européenne", "Santé & Bien-être", "Science", "Astronomie & Espace",
 ]
 
 
